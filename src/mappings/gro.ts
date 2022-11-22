d3d7456d18ae82c19ff55ebdb019a5c116e2771c
Sergi Juanati <wint3rmute@gmail.com>
2022-11-22 17:29:41 +0100

    Updated manifest, template and ABIs

---
+++ src/mappings/gro.ts
@@ -0,0 +1,12 @@
+import {
+  Approval,
+  Transfer,
+} from '../../../../old_bcollective/generated/gro/ERC20';
+
+export function handleApproval(event: Approval): void {
+  // do something
+}
+
+export function handleTransfer(event: Transfer): void {
+  // do something
+}
